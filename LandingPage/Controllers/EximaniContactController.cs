c7bffa8588ffb6ce67206aa0a6793e33a2456f14
vansonnguyen159 <vansonnguyen159@outlook.com>
2021-04-19 01:46:27 +0700

    web config

--- LandingPage/Controllers/EximaniContactController.cs
+++ LandingPage/Controllers/EximaniContactController.cs
@@ -37,17 +37,17 @@ namespace LandingPage.Controllers
             try
             {
                 var customerContact = await _contactService.SaveCustomerContact(contactdto);
-                //var mailContent = $@"
-                //            <div><h4>Contact Information</h4></div>
-                //            <div><strong>ContactId: </strong>{customerContact.Id}</div>
-                //            <div><strong>Customer Name: </strong>{customerContact.FirstName} {customerContact.LastName}</div>
-                //            <div><strong>Email: </strong>{customerContact.Email}</div>
-                //            <div><strong>PhoneNumber: </strong>{customerContact.PhoneNumber}</div>
-                //            <div><strong>Message: </strong>{customerContact.Message}</div>
-                //            <div><strong>Sent Date: </strong>{customerContact.CreatedDate.Value.ToString("dd/MM/yyyy HH:mm:ss")}</div>
-                //        ";
-                //var message = new Message("Khách đăng kí nhận thông tin", mailContent);
-                //await _emailSender.SendEmailAsync(message);
+                var mailContent = $@"
+                            <div><h4>Contact Information</h4></div>
+                            <div><strong>ContactId: </strong>{customerContact.Id}</div>
+                            <div><strong>Customer Name: </strong>{customerContact.FirstName} {customerContact.LastName}</div>
+                            <div><strong>Email: </strong>{customerContact.Email}</div>
+                            <div><strong>PhoneNumber: </strong>{customerContact.PhoneNumber}</div>
+                            <div><strong>Message: </strong>{customerContact.Message}</div>
+                            <div><strong>Sent Date: </strong>{customerContact.CreatedDate.Value.ToString("dd/MM/yyyy HH:mm:ss")}</div>
+                        ";
+                var message = new Message("Khách đăng kí nhận thông tin", mailContent);
+                await _emailSender.SendEmailAsync(message);
                 return Json(new { Status = 200, Message = "Success" });
             }
             catch (Exception)
